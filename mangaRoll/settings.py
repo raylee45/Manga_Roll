f2cf9fba247ab782d0e0c658f25962bd9c1fe87e
raylee45 <raylee45@gmail.com>
2023-01-10 17:49:01 -0800

    settings.py 121

--- mangaRoll/settings.py
+++ mangaRoll/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-STATIC_URL = 'static/'
+STATIC_URL = 'mangaRoll_app/static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mangaRoll_app/static')
