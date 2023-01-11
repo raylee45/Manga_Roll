b2777d8eac2faafbdf0bfccc263769e104846c37
raylee45 <raylee45@gmail.com>
2023-01-10 17:18:29 -0800

    empty test commit 2

--- mangaRoll/settings.py
+++ mangaRoll/settings.py
@@ -15,7 +15,7 @@ print(BASE_DIR)
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.get('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 print(environ)
 print('secret key = ', SECRET_KEY)
 
